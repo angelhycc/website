b8d936c277ea0d4c640b94fb669e37780801bd9d
angelhyc <angelhyc@umich.edu>
2024-01-22 22:50:46 -0500

    change homepath

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <>
     <Routes>
     {/* <Route path='test' element={<LandingPage/>}/> */}
-    <Route path='/' element={<Layout/>} >
+    <Route path='/aboutme' element={<Layout/>} >
       <Route index element={<Home />} />
       <Route path='about' element={<About />}/>
       <Route path='projects' element={<Projects/>}>
